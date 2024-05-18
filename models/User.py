333d307366ecc9f5f33743085bebf132d81efed8
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-05-18 17:37:04 +0800

    Conflitct resolved

--- models/User.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This modul contains the User Class"""
-from .base_model import BaseModel
-
-class User(BaseModel):
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
