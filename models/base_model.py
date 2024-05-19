38ba145269cca3cc16273154cff53785d8c85546
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-05-19 15:08:56 +0800

    modified the to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,13 @@ time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
     """
-    This class will be the base model
+        This class will the base model
     """
 
     def __init__(self, *args, **kwargs):
-        """It will initiate the object"""
+        """
+            It will initiate the object
+        """
         if kwargs:
             for k, v in kwargs.items():
                 if k == '__class__':
@@ -26,12 +28,12 @@ class BaseModel:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
             if hasattr(self, "updated_at") and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """Returns the string representation"""
@@ -50,10 +52,9 @@ class BaseModel:
         First piece of the serialization/deserialization process
         create dictionary representation of our class (BaseModel)
         """
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        return new_dict
+        name = self.__class__.__name__
+        New_dict = self.__dict__.copy()
+        New_dict.update(__class__=name, created_at=self.created_at.isoformat())
+        New_dict.update(updated_at=self.updated_at.isoformat())
+
+        return New_dict
